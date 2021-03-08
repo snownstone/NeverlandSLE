44be8cbabc10cd198954b68401779a7c70b84385
snownstone <yanglq@outlook.com>
2021-03-08 18:30:30 +0800

    暂不对fft后的y做log转换

--- AA_realtime_audiofft.py
+++ AA_realtime_audiofft.py
@@ -26,7 +26,7 @@ stream = pa.open(format=pyaudio.paInt16, channels=1, rate=fs, input=True, frames
 
 end_index = []
 for i in np.arange(12):    
-    end_index.append(int((buff_size/2)/2**i))
+    end_index.append(int((buff_size/2)/2**i)-1)
 end_index.reverse()
 
     
@@ -50,18 +50,8 @@ while True:
 
     X2 = []
     for i in np.arange(12):
-        XX = np.mean(X[start_index[i]:end_index[i]])
-        # XX 可能为 0，则下面无法求 log10，所以为 0 时，令其等于一个非常小的值
-        if XX == 0:
-            XX = 1e-300
-        # 下面是将振幅转为分贝，就是以10为底取对数，因为相比线性关系
-        # 对数关系更符合人耳对声音响度的辨别机制 
-        # 取绝对值是因为分贝尺度中，符号没影响
-        
-        XXdB = 20*np.log10(XX)
-        XXout = np.abs(XXdB)
-        X2.append(XXout)
-        #X2.append(XX)
+        XX = np.mean(X[start_index[i]:end_index[i]])        
+        X2.append(XX)
         i=i+1
     
     outlist = ['X2_12',X2]
